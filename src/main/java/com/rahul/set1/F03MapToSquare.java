35b163b71c98de09108ac8e554ed338e6f8bfaad
itsrahulchaudhary <rahulcy49@gmail.com>
2025-01-03 21:18:06 +0530

    stream basic question

--- src/main/java/com/rahul/set1/F03MapToSquare.java
+++ src/main/java/com/rahul/set1/F03MapToSquare.java
@@ -3,7 +3,7 @@ package com.rahul.set1;
 import java.util.Arrays;
 import java.util.List;
 // Write a Java 8 program to map integers to their squares and print results.
-public class MapToSquare {
+public class F03MapToSquare {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
         numbers.stream().map(i -> i*i).forEach(System.out::println);
